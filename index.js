bf3cbd02f26f2e866bfd1cd7f05e69122f8a6ad8
gopal1311 <gopalcontractor17@gmail.com>
2017-08-28 23:43:19 +0530

    gopal1321111d

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var express = require('express')
 var app=express()
 var path =require('path')
 app.use('/css',express.static(__dirname + '/css'))
-app.set('port', (process.env.PORT || 5000));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 app.set('view engine', 'html');
 
 app.get('/',function(req,res){
