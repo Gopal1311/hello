d2bf5a0c3cf1e421a8d82108cdfe5605a8ead983
gopal1311 <gopalcontractor17@gmail.com>
2017-08-28 23:28:30 +0530

    gopal1321111

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ var express = require('express')
 
 var app=express()
 var path =require('path')
-
-app.set('port', (process.env.PORT || 5000));
-
 app.use('/css',express.static(__dirname + '/css'))
+app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'html');
 
-app.get('/', function(request, response) {
-  response.render('index.html');
-});
-
-// app.get('/',function(req,res){
-
-// 	res.sendFile('index.html',{root:path.join(__dirname)})
-// })
+app.get('/',function(req,res){
+res.render('main');
+//	res.sendFile('index.html',{root:path.join(__dirname)})
+})
