017154ce780a3a91039d13c397ae1d1965dc70c0
gopal1311 <gopalcontractor17@gmail.com>
2017-08-28 23:45:21 +0530

    gopal1321111ds

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.use('/css',express.static(__dirname + '/css'))
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-app.set('view engine', 'html');
+
 
 app.get('/',function(req,res){
-res.render('main');
-//	res.sendFile('index.html',{root:path.join(__dirname)})
+//res.render('main');
+	res.sendFile('index.html',{root:path.join(__dirname)})
 })
